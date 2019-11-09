bd1d0eabf8afc9913f03ad130d47317bebbbd84d
Tatu <tatu.holkko@gmail.com>
2019-11-09 02:30:34 +0200

    add first methods in game class

--- game.cpp
+++ game.cpp
@@ -1,6 +1,17 @@
 #include "game.h"
 
-Game::Game()
+Game::Game(int size)
 {
-
+    for (int i = 0; i < size; i++){
+        board_.push_back(vector<int>(size));
+    }
 }
+
+bool Game::place(int unit, int x, int y){
+    if (board_.at(y).at(x) != 0){
+        return false;
+    }
+    board_.at(y).at(x) = unit;
+    return true;
+}
+
--- game.h
+++ game.h
@@ -1,11 +1,35 @@
 #ifndef GAME_H
 #define GAME_H
+#include <vector>
 
+using namespace std;
 
 class Game
 {
 public:
-    Game();
+    Game(int size);
+    //side length of the game board
+    int size_ = 0;
+    //return game state as an enumerator of type State
+    void get_state();
+
+private:
+
+    //grid where the units are represented as integers
+    vector<vector<int>> board_;
+
+    /* place a unit into the board
+     * return true if the move was legal and executed
+     */
+    bool place(int unit, int x, int y);
+
+    //the game is always in one of these states
+    enum State{
+        player1_win,
+        player2_win,
+        tied,
+        ongoing
+    };
 };
 
 #endif // GAME_H
