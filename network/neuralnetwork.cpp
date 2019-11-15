2e1fc94ebc841cb940bf4424e7c65afe3b7c897a
Tatu <tatu.holkko@gmail.com>
2019-11-15 15:08:32 +0200

    add neural network constructor

--- network/neuralnetwork.cpp
+++ network/neuralnetwork.cpp
@@ -1,6 +1,31 @@
 #include "neuralnetwork.h"
+#include <cmath>
 
-neuralnetwork::neuralnetwork()
+neuralnetwork::neuralnetwork(int grid_diameter,
+                             int kernel_radius,
+                             int hidden_neurons,
+                             int mutation_rate,
+                             int number_of_kernels):
+    grid_size_(pow(grid_diameter, 2)),
+    number_of_kernels_(number_of_kernels),
+    kernel_radius_(kernel_radius),
+    kernel_side_(2*kernel_radius + 1),
+    hidden_layer_size_(hidden_neurons),
+    mutation_rate_(mutation_rate)
+
 {
-
+    //initialize weights to zero
+    for (int i = 0; i < number_of_kernels; i++){
+        vector<float> kernel(0, pow(kernel_side_, 2));
+        kernels_.push_back(kernel);
+    }
+    for (int neuron = 0; neuron < hidden_neurons; neuron++){
+        vector<float> convoluted_inputs(0, pow(grid_diameter - 2*kernel_radius, 2)*number_of_kernels);
+        hidden_layer_.push_back(convoluted_inputs);
+    }
+    for (int output = 0; output < pow(grid_diameter,2); output++){
+        vector<float> hidden_layer_output(0, hidden_neurons);
+        output_.push_back(hidden_layer_output);
+    }
 }
+
--- network/neuralnetwork.h
+++ network/neuralnetwork.h
@@ -7,7 +7,11 @@ using namespace std;
 class neuralnetwork
 {
 public:
-    neuralnetwork();
+    neuralnetwork(int grid_diameter,
+                  int kernel_radius,
+                  int hidden_neurons,
+                  int mutation_rate,
+                  int number_of_kernels);
     /* feed game board state to the network and calculate the output
      * probability distribution where this network would place it's
      * next unit
@@ -20,20 +24,22 @@ public:
 private:
     //game grid side length
     int grid_size_ = 0;
-    //radius of the kernel
+    //number of kernels
+    int number_of_kernels_ = 0;
+    //radius of the kernel (excluding center)
     int kernel_radius_ = 0;
     //side length of the kernel
     int kernel_side_ = 0;
     //number of neurons in the hidden layer
     int hidden_layer_size_ = 0;
     //this scales the random nudges in weights
-    float mutation_rate = 0;
+    float mutation_rate_ = 0;
     //kernel weights for convolution
-    vector<vector<float>> kernels_;
+    vector<vector<float>> kernels_ = {};
     //hidden layer weights
-    vector<float> hidden_layer;
+    vector<vector<float>> hidden_layer_ = {};
     //output weights
-    vector<float> output;
+    vector<vector<float>> output = {};
     //activation function
     float sigmoid(float x);
 };
